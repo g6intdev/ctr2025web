650378fd1f3e0b4a12da8a7429a2ffcbba1493d4
g6intdev <134787075+g6intdev@users.noreply.github.com>
2025-08-20 05:07:15 +0800

    rename groupName to teamName

--- src/lib/registerUserAndReturn.ts
+++ src/lib/registerUserAndReturn.ts
@@ -1,13 +1,24 @@
 import prisma from "@/lib/prisma"
 
-export default async function registerUserAndReturn(name: string, course: "serious" | "fun", category: "indiv" | "team", groupName?: string) {
-  const user = await prisma.user.create({
-    data: {
-      name: name,
-      runnerCourse: course,
-      runnerCategory: category,
-      groupName: groupName
-    }
-  })
-  return user
+export default async function registerUserAndReturn(name: string, course: "serious" | "fun", category: "indiv" | "team", teamName?: string) {
+  if (teamName) {
+    const user = await prisma.user.create({
+      data: {
+        name: name,
+        runnerCourse: course,
+        runnerCategory: category,
+        teamName: teamName
+      }
+    })
+    return user
+  } else {
+    const user = await prisma.user.create({
+      data: {
+        name: name,
+        runnerCourse: course,
+        runnerCategory: category,
+      }
+    })
+    return user
+  }
 }
